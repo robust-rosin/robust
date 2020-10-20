673d8c594b14ea8ed391ff36adf24891ced8071d
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2020-10-20 10:08:37 +0200

    run bash script from anywhere

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # usage: ./validate.sh bugid.bug
-
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+VALIDATOR="${DIR}/yamale_validator.py"
 echo Checking schema compliance for $1
-python3 yamale_validator.py $1
+python3 ${VALIDATOR} $1
