52f6a222b148d0aa0d2ae2bf55ef829c35181740
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2020-11-03 17:17:13 +0100

    addresses review comments

--- scripts/gdocs_reader.py
+++ scripts/gdocs_reader.py
@@ -20,6 +20,10 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 SPREADSHEET_ID = '1WMnp8a2I79aVBS4x_vjAkxFzzELsBpmjYgMEuytZOhg'
 RANGE_NAME = 'Coding!A2:F'
 
+COLUMN_BUG = 0
+COLUMN_FAULT = 4
+COLUMN_FAILURE = 5
+
 dir_here = os.path.dirname(__file__)
 token_path = os.path.join(dir_here, 'token.pickle')
 credentials_path = os.path.join(dir_here, 'credentials.json')
@@ -56,17 +60,17 @@ def get_gsheets_data() -> t.Iterator[t.List[str]]:
 
     if not values:
         print('Error: No data found.')
-    else:
-        for row in values:
-            # return columns 'Bug', 'Fault labels' and 'Failure labels',
-            # which correspond to indices 0, 4 and 5.
-            try:
-                yield [row[0], row[4], row[5]]
-            except:
-                print(f"ERROR: Empty fields encountered for {row[0]} in 'ROBUST coding'")
-                yield [row[0], "", ""]
+        return
 
-def refactor_fault_failure(description: t.List[str], faults: str, failures: str, update: bool = True) -> None:
+    for row in values:
+        # return columns 'Bug', 'Fault labels' and 'Failure labels'
+        try:
+            yield [row[COLUMN_BUG], row[COLUMN_FAULT], row[COLUMN_FAILURE]]
+        except:
+            print(f"ERROR: Empty fields encountered for {row[COLUMN_BUG]} in 'ROBUST coding'")
+            yield [row[COLUMN_BUG], "", ""]
+
+def refactor_fault_failure(description: t.List[str], faults: t.List[str], failures: t.List[str], update: bool = True) -> None:
     if 'failure-codes' and 'fault-codes' in description:
         if not update:
             return
@@ -74,8 +78,8 @@ def refactor_fault_failure(description: t.List[str], faults: str, failures: str,
             del description['fault-codes']
             del description['failure-codes']
 
-    description['fault-codes'] = failures.upper().splitlines()
-    description['failure-codes'] = faults.upper().splitlines()
+    description['fault-codes'] = failures
+    description['failure-codes'] = faults
 
 def refactor_file(filename: str, faults: str, failures: str) -> None:
     robust_dir = os.path.dirname(dir_here)
@@ -85,11 +89,13 @@ def refactor_file(filename: str, faults: str, failures: str) -> None:
         with open(bug_path, 'r') as f:
             description = yaml.load(f)
     except (FileNotFoundError, IOError) as err:
-        print((f"{err}"))
+        print(f"{err}")
         return
 
     try:
-        refactor_fault_failure(description, faults, failures)
+        faults_ = faults.upper().splitlines()
+        failures_ = failures.upper().splitlines()
+        refactor_fault_failure(description, faults_, failures_)
     except ValueError as err:
         print(f"ERROR [{filename}]: {err}")
 
